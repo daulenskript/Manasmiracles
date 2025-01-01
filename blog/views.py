7722b279ebb114de054da0580d4b348dc2412a87
daulenskript <kazbekok7@gmail.com>
2025-01-02 01:27:02 +0600

    post func

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView , DetailView , CreateView
 from .models import Post
 # Create your views here.
 
 
 def home(request):
-    
-    return render(request , 'blog/home.html' )
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request , 'blog/home.html' , context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title' , 'content']
+
 def about(request):
  
     return render(request , 'blog/about.html' )
