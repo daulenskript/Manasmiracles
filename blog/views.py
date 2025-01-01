db077b94c0b50e9b879124ac6193f033bb02e66e
daulenskript <kazbekok7@gmail.com>
2025-01-02 01:55:50 +0600

    post update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView , DetailView , CreateView
+from django.views.generic import ListView , DetailView , CreateView , UpdateView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 # Create your views here.
 
 
@@ -20,10 +21,24 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin , CreateView):
     model = Post
     fields = ['title' , 'content']
+    def form_valid(self , form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin , UpdateView):
+    model = Post
+    fields = ['title' , 'content']
+    def form_valid(self , form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request == post.author:
+            return True
+        return False
 def about(request):
  
     return render(request , 'blog/about.html' )
